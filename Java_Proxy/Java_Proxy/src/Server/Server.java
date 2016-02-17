787435012a8091adf454cc882e3ec91ab477654a
Gogurt <garrett.leatherman@ndsu.edu>
2016-02-17 12:25:52 -0600

    Start of query interpretation
    Isolated the search query url for the Java HTTP class to use in its GET
    method.

--- Java_Proxy/Java_Proxy/src/Server/Server.java
+++ Java_Proxy/Java_Proxy/src/Server/Server.java
@@ -3,13 +3,19 @@ import java.awt.List;
 import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.Scanner;
+
+import java.net.HttpURLConnection;
 
 /* Simple Java Proxy
  * Listens for additional connecting clients,
@@ -61,18 +67,47 @@ class ClientSocketThread extends Thread {
 		    char[] buffer = new char[2048];
 		    int charsRead = 0;
 		    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		    String queryRequest = null;
+		    boolean queryDetected = false;
 		    while ((charsRead = in.read(buffer)) != -1)
 		    {
 		        String message = new String(buffer).substring(0, charsRead);
-		        System.out.println(message);
+		        if(message.contains("query=")) {
+		        	System.out.println("Query detected!");
+		        	System.out.println(message.substring(message.indexOf("query="), message.length()));
+		        	queryRequest = message.substring(message.indexOf("query="), message.length());
+		        	System.out.println(queryRequest.substring(6, queryRequest.length() - 25));
+		        	queryDetected = false; //Only false for debugging purposes
+		        }
 		    }
-		    
-		    //This is where the thread will listen to the connected client
-			//in order to send and receive web page information,
-			//as well as verifying/adding the webpage as a cache object.
+		    if(queryDetected) {
+		    	//HTTP GET occurs
+		    	
+		    	//URL obj = new URL(queryRequest.substring(7, queryRequest.indexOf("=") - 25));
+				//HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+				
+				// optional default is GET
+				//con.setRequestMethod("GET");
+		    	
+		    	
+		    	//Check to see if currently connected page was recently visited,
+		    	//if not, store on the proxy host.
+		    }
 		
 		} finally {
 			clientSocket.close();
 		}
+	}
+}
+
+class WebpageObject {
+	private String url;
+	private String title;
+	private String contents;
+
+	public WebpageObject(String url, String title, String contents) {
+		this.url = url;
+		this.title = title;
+		this.contents = contents;
 	}
 }
