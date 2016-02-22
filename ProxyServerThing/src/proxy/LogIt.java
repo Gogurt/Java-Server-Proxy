0d77fd8a8f67bbae19a4bab57d9433485817d8bd
Matthew Leet <matthew11leet@gmail.com>
2016-02-22 11:30:41 -0600

    J meltons tutorial

---
+++ ProxyServerThing/src/proxy/LogIt.java
@@ -0,0 +1,50 @@
+/* To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proxy;
+
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+//import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Matthew
+ */
+public class LogIt 
+{
+    public LogIt()
+    {
+    }
+    
+    String logFile = "logRequests.txt";
+    //SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:ms");
+    Date date = new Date();
+    
+    public synchronized void logRequest(String clientURL, String clientIP) throws IOException 
+    {
+        
+        try
+        {
+            PrintWriter outputStream = new PrintWriter(new FileWriter(logFile, true));
+            outputStream.write("\r\n" + "Date: " + date + "  URL: " + clientURL + "  Client IP: " + clientIP);
+            
+            
+            outputStream.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.print("Error writing to log file");
+            e.printStackTrace();
+        }
+                
+                
+        
+    }
+    
+    
+}
